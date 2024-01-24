0c3a765cf9a37228d8ad5f0124691205d65093c9
rakesh Guddeti <raki64846@gmail.com>
2024-01-24 17:33:40 +0530

    .

--- 01-print/main.tf
+++ 01-print/main.tf
@@ -15,5 +15,5 @@ output "colour2" {
 }
 
 output "fruits" {
-  value = "The qty of banana is-${lookup(var.fruits, "banana", "no banana").qty}"
+  value = "The qty of banana is-${lookup(var.fruits[banana], "qty", "no banana")}"
 }
