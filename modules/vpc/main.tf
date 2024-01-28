e10386731b33aec188ed9db8b3eec12d09e265c2
rakesh Guddeti <raki64846@gmail.com>
2024-01-28 19:25:56 +0530

    .

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "main" {
   }
 }
 
-resource "aws_vpc_peering_connection" "foo" {
+resource "aws_vpc_peering_connection" "main" {
   peer_vpc_id   = aws_vpc.main.id
   vpc_id        = data.aws_vpc.default.id
   auto_accept   = true
@@ -24,4 +24,16 @@ resource "aws_vpc_peering_connection" "foo" {
   tags = {
     Name = "${var.env}-vpc-with-default-vpc"
   }
+}
+
+resource "aws_route" "main" {
+  route_table_id            = aws_vpc.main.main_route_table_id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+}
+
+resource "aws_route" "default-vpc" {
+  route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = aws_vpc.main.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
 }
