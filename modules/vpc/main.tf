118615f6435e9ee2f4f6292f7ca1954440cfd956
rakesh Guddeti <raki64846@gmail.com>
2024-01-29 00:20:20 +0530

    .

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -60,7 +60,8 @@ resource "aws_route_table_association" "public" {
 }
 
 resource "aws_eip" "main" {
-  domain = ""
+  count          = length(var.public_subnets_cidr)
+  domain         = "vpc"
 }
 resource "aws_nat_gateway" "main" {
   count          = length(var.public_subnets_cidr)
