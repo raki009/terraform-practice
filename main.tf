218c28061f7d10f702e8c073599d9ceb0af263a3
rakesh Guddeti <raki64846@gmail.com>
2024-01-29 18:28:04 +0530

    .

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ module "vpc" {
   vpc_cidr      = lookup(each.value, "vpc_cidr", null)
   public_subnets_cidr  = lookup(each.value, "public_subnets_cidr", null)
   web_subnets_cidr = lookup(each.value, "web_subnets_cidr", null)
-  app_subnets_cidr = lookup(each.value, "app_subnets_cidr", null)
-  db_subnets_cidr = lookup(each.value, "db_subnets_cidr", null)
+#  app_subnets_cidr = lookup(each.value, "app_subnets_cidr", null)
+#  db_subnets_cidr = lookup(each.value, "db_subnets_cidr", null)
   az            = lookup(each.value, "az", null)
 
 
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -4,8 +4,8 @@ variable "project_name" {}
 variable "vpc_cidr" {}
 variable "public_subnets_cidr" {}
 variable "web_subnets_cidr" {}
-variable "app_subnets_cidr" {}
-variable "db_subnets_cidr" {}
+#variable "app_subnets_cidr" {}
+#variable "db_subnets_cidr" {}
 variable "az" {}
 
 
