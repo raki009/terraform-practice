eb0e5f698e46c0d863b8432875c505bdda194dcd
rakesh Guddeti <raki64846@gmail.com>
2024-01-29 18:35:39 +0530

    .

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -59,75 +59,75 @@ resource "aws_route_table_association" "public" {
   subnet_id      = lookup(element(aws_subnet.public_subnets, count.index), "id", null)
 }
 
-#resource "aws_subnet" "app_subnets" {
-#  count      = length(var.app_subnets_cidr)
-#  vpc_id     = aws_vpc.main.id
-#  cidr_block = element(var.app_subnets_cidr, count.index)
-#
-#  tags = {
-#    Name = "app-subnet-${count.index + 1}"
-#  }
-#}
-#
-#resource "aws_route_table" "app" {
-#  count = length(var.app_subnets_cidr)
-#  vpc_id = aws_vpc.main.id
-#
-#  route {
-#    cidr_block = "0.0.0.0/0"
-#    gateway_id = aws_internet_gateway.main.id
-#  }
-#
-#  route {
-#    cidr_block                = data.aws_vpc.default.cidr_block
-#    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
-#  }
-#
-#  tags = {
-#    Name = "app-rt-${count.index + 1}"
-#  }
-#}
-#
-#resource "aws_route_table_association" "app" {
-#  count          = length(var.app_subnets_cidr)
-#  route_table_id = lookup(element(aws_route_table.app, count.index), "id", null)
-#  subnet_id      = lookup(element(aws_subnet.app_subnets, count.index), "id", null)
-#}
-#
-#resource "aws_subnet" "db_subnets" {
-#  count      = length(var.db_subnets_cidr)
-#  vpc_id     = aws_vpc.main.id
-#  cidr_block = element(var.db_subnets_cidr, count.index)
-#
-#  tags = {
-#    Name = "db-subnet-${count.index + 1}"
-#  }
-#}
-#
-#resource "aws_route_table" "db" {
-#  count = length(var.db_subnets_cidr)
-#  vpc_id = aws_vpc.main.id
-#
-#  route {
-#    cidr_block = "0.0.0.0/0"
-#    gateway_id = aws_internet_gateway.main.id
-#  }
-#
-#  route {
-#    cidr_block                = data.aws_vpc.default.cidr_block
-#    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
-#  }
-#
-#  tags = {
-#    Name = "db-rt-${count.index + 1}"
-#  }
-#}
-#
-#resource "aws_route_table_association" "db" {
-#  count          = length(var.db_subnets_cidr)
-#  route_table_id = lookup(element(aws_route_table.db, count.index), "id", null)
-#  subnet_id      = lookup(element(aws_subnet.db_subnets, count.index), "id", null)
-#}
+resource "aws_subnet" "app_subnets" {
+  count      = length(var.app_subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.app_subnets_cidr, count.index)
+
+  tags = {
+    Name = "app-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "app" {
+  count = length(var.app_subnets_cidr)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  route {
+    cidr_block                = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+
+  tags = {
+    Name = "app-rt-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table_association" "app" {
+  count          = length(var.app_subnets_cidr)
+  route_table_id = lookup(element(aws_route_table.app, count.index), "id", null)
+  subnet_id      = lookup(element(aws_subnet.app_subnets, count.index), "id", null)
+}
+
+resource "aws_subnet" "db_subnets" {
+  count      = length(var.db_subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.db_subnets_cidr, count.index)
+
+  tags = {
+    Name = "db-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "db" {
+  count = length(var.db_subnets_cidr)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  route {
+    cidr_block                = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+
+  tags = {
+    Name = "db-rt-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table_association" "db" {
+  count          = length(var.db_subnets_cidr)
+  route_table_id = lookup(element(aws_route_table.db, count.index), "id", null)
+  subnet_id      = lookup(element(aws_subnet.db_subnets, count.index), "id", null)
+}
 
 resource "aws_eip" "main" {
   count          = length(var.public_subnets_cidr)
