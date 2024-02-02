1851d4722ae1f6a24482bad48207333091de79d4
rakesh Guddeti <raki64846@gmail.com>
2024-02-02 17:40:29 +0530

    .

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -57,3 +57,20 @@ resource "aws_lb_listener" "https" {
     target_group_arn = var.target_group_arn
   }
 }
+
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
